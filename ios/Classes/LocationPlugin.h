1fdc7970938ff7ff371b0aedec8b1f8674a6eddf
coolxinxin <xinxiniscool@gmail.com>
2022-05-21 09:35:26 +0800

    修复ios编译报错的问题

--- ios/Classes/LocationPlugin.h
+++ ios/Classes/LocationPlugin.h
@@ -1,4 +1,7 @@
 #import <Flutter/Flutter.h>
 
+#import <CoreLocation/CoreLocation.h>
+
 @interface LocationPlugin : NSObject<FlutterPlugin>
+@property (nonatomic, strong) CLGeocoder *geocoder;
 @end
